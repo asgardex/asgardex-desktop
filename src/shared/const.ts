3dc5464aab83f28c5bd3c08dbc58b9d52d12812f
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2023-08-11 10:21:43 +0930

    udpate to new repo asgardex-desktop

--- src/shared/const.ts
+++ src/shared/const.ts
@@ -19,8 +19,8 @@ export const NINE_REALMS_CLIENT_HEADER = 'x-client-id'
 export enum ExternalUrl {
   DOCS = 'https://docs.thorchain.org',
   DISCORD = 'https://discord.gg/pHcS67yX7Z',
-  GITHUB_REPO = `https://github.com/thorchain/asgardex-electron`,
-  GITHUB_RELEASE = `https://github.com/thorchain/asgardex-electron/releases/tag/v`,
+  GITHUB_REPO = `https://github.com/thorchain/asgardex-desktop`,
+  GITHUB_RELEASE = `https://github.com/thorchain/asgardex-desktop/releases/tag/v`,
   TWITTER = 'https://twitter.com/asgardex'
 }
 
