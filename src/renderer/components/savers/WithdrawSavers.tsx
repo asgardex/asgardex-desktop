fa9369d38ddb452f9cda2325a16d80f49f11113b
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2023-09-08 10:46:24 +0930

    update chain msg

--- src/renderer/components/savers/WithdrawSavers.tsx
+++ src/renderer/components/savers/WithdrawSavers.tsx
@@ -1330,7 +1330,7 @@ export const WithdrawSavers: React.FC<WithDrawProps> = (props): JSX.Element => {
                     </div>
                     <div className="flex w-full justify-between pl-10px text-[12px]">
                       <div className={`flex items-center`}>
-                        {intl.formatMessage({ id: 'common.confirmation.time' })}
+                        {intl.formatMessage({ id: 'common.confirmation.time' }, { chain: sourceAsset.chain })}
                       </div>
                       <div>{formatSwapTime(Number(transactionTime.confirmation))}</div>
                     </div>
