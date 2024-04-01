5b36bf69195caf47461d1ffeba381de42606fbe0
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2024-04-02 01:52:55 +0200

    Create balances.ts

---
+++ src/renderer/services/arb/balances.ts
@@ -0,0 +1,64 @@
+import { Network } from '@xchainjs/xchain-client'
+import { Asset } from '@xchainjs/xchain-util'
+import * as A from 'fp-ts/lib/Array'
+import * as FP from 'fp-ts/lib/function'
+
+import { HDMode, WalletType } from '../../../shared/wallet/types'
+import { ArbAssetsTestnet } from '../../const'
+import { validAssetForARB } from '../../helpers/assetHelper'
+import { liveData } from '../../helpers/rx/liveData'
+import { observableState } from '../../helpers/stateHelper'
+import * as C from '../clients'
+import { client$ } from './common'
+
+/**
+ * `ObservableState` to reload `Balances`
+ * Sometimes we need to have a way to understand if it simple "load" or "reload" action
+ * e.g. @see src/renderer/services/wallet/balances.ts:getChainBalance$
+ */
+const { get$: reloadBalances$, set: setReloadBalances } = observableState<boolean>(false)
+
+const resetReloadBalances = () => {
+  setReloadBalances(false)
+}
+
+const reloadBalances = () => {
+  setReloadBalances(true)
+}
+
+// State of balances loaded by Client
+const balances$: ({
+  walletType,
+  network,
+  walletIndex,
+  hdMode
+}: {
+  walletType: WalletType
+  network: Network
+  walletIndex: number
+  hdMode: HDMode
+}) => C.WalletBalancesLD = ({ walletType, walletIndex, network, hdMode }) => {
+  // For testnet we limit requests by using pre-defined assets only
+  const assets: Asset[] | undefined = network === Network.Testnet ? ArbAssetsTestnet : undefined
+  return FP.pipe(
+    C.balances$({
+      client$,
+      trigger$: reloadBalances$,
+      assets,
+      walletType,
+      walletIndex,
+      hdMode,
+      walletBalanceType: 'all'
+    }),
+    // Filter assets based on ARBERC20Whitelist (mainnet only)
+    liveData.map(FP.flow(A.filter(({ asset }) => validAssetForARB(asset, network))))
+  )
+}
+
+// State of balances loaded by Client and Address
+const getBalanceByAddress$ = (network: Network) => {
+  const assets: Asset[] | undefined = network === Network.Testnet ? ArbAssetsTestnet : undefined
+  return C.balancesByAddress$({ client$, trigger$: reloadBalances$, assets, walletBalanceType: 'all' })
+}
+
+export { reloadBalances, balances$, reloadBalances$, resetReloadBalances, getBalanceByAddress$ }
