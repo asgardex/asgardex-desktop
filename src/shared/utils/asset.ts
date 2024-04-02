2bdc4425ace763d39bdf7f1adfd5eae846ccdcf8
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2024-04-02 02:42:46 +0200

    Update asset.ts

--- src/shared/utils/asset.ts
+++ src/shared/utils/asset.ts
@@ -1,3 +1,4 @@
+import { AssetARB } from '@xchainjs/xchain-arbitrum'
 import { AssetAVAX } from '@xchainjs/xchain-avax'
 import { AssetBNB } from '@xchainjs/xchain-binance'
 import { AssetBTC } from '@xchainjs/xchain-bitcoin'
@@ -25,6 +26,7 @@ const AssetSynthEth = assetFromStringEx('ETH/ETH')
 const AssetSynthEthUsdc = assetFromStringEx('ETH/USDC-0XA0B86991C6218B36C1D19D4A2E9EB0CE3606EB48')
 const AssetSynthEthUsdt = assetFromStringEx('ETH/USDT-0XDAC17F958D2EE523A2206206994597C13D831EC7')
 const AssetSynthLTC = assetFromStringEx('LTC/LTC')
+const AssetSynthARB = assetFromStringEx('ARB/ETH')
 const AssetSynthAVAX = assetFromStringEx('AVAX/AVAX')
 const AssetSynthAVAXUSDC = assetFromStringEx('AVAX/USDC-0XB97EF9EF8734C71904D8002F8B6BC66DD9C48A6E')
 const AssetSynthATOM = assetFromStringEx('GAIA/ATOM')
@@ -44,6 +46,7 @@ export {
   AssetSynthBnb,
   AssetSynthBusd,
   AssetSynthLTC,
+  AssetSynthARB,
   AssetSynthAVAX,
   AssetSynthAVAXUSDC,
   AssetSynthDOGE,
@@ -59,6 +62,7 @@ export {
   AssetLTC,
   AssetDOGE,
   AssetBSC,
+  AssetARB,
   AssetAVAX,
   AssetETH,
   AssetRune67C,
