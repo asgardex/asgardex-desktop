2f5fc939836f9c2b081072ca18b8f83c065b6b05
AlphaEdgeXBT <rodeos.diploid_0l@icloud.com>
2024-08-28 20:48:49 +0200

    Fix typo #335

--- src/renderer/components/settings/WalletSettings.tsx
+++ src/renderer/components/settings/WalletSettings.tsx
@@ -376,7 +376,7 @@ export const WalletSettings: React.FC<Props> = (props): JSX.Element => {
                       <InfoIcon
                         tooltip={intl.formatMessage(
                           { id: 'setting.wallet.hdpath.legacy.info' },
-                          { path: getEvmDerivationPath(walletAccountMap[chain], walletIndexMap[chain], 'ledgerlive') }
+                          { path: getEvmDerivationPath(walletAccountMap[chain], walletIndexMap[chain], 'legacy') }
                         )}
                       />
                     </Styled.EthDerivationModeRadioLabel>
