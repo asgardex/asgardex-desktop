90e918621ebc4c0596326dd8ed82a882e8cc4b68
cinnamoroll6130 <nextmission4260@gmail.com>
2024-10-08 21:03:52 -0400

    remove duplicated colors for chart

--- src/renderer/components/wallet/assets/TotalAssetValue.tsx
+++ src/renderer/components/wallet/assets/TotalAssetValue.tsx
@@ -9,12 +9,12 @@ import {
   baseToAsset,
   formatAssetAmountCurrency
 } from '@xchainjs/xchain-util'
-// import { useIntl } from 'react-intl'
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts'
 
 import { AssetUSDC } from '../../../const'
 import { hiddenString } from '../../../helpers/stringHelper'
 import { BaseButton } from '../../uielements/button'
+import { ChartColors } from '../../uielements/chart/utils'
 import { InfoIcon } from '../../uielements/info'
 import * as Styled from './TotalValue.styles'
 
@@ -32,33 +32,10 @@ export const TotalAssetValue: React.FC<Props> = (props): JSX.Element => {
 
   const [showDetails, setShowDetails] = useState<boolean>(false)
   const chartData = useMemo(() => {
-    const COLORS = [
-      '#0088FE', // Vivid Blue
-      '#00C49F', // Aqua Green
-      '#FFBB28', // Vibrant Yellow
-      '#FF8042', // Bright Orange
-      '#A569BD', // Purple
-      '#F4D03F', // Sunflower Yellow
-      '#5DADE2', // Light Blue
-      '#48C9B0', // Medium Aquamarine
-      '#EC7063', // Soft Red
-      '#AF7AC5', // Lavender
-      '#F7DC6F', // Light Goldenrod Yellow
-      '#82E0AA', // Pastel Green
-      '#F5B041', // Tangerine Yellow
-      '#85C1E9', // Sky Blue
-      '#D7DBDD', // Light Gray
-      '#AED6F1', // Pale Blue
-      '#A3E4D7', // Pale Aqua Green
-      '#FAD7A0', // Peach Orange
-      '#F5CBA7', // Light Brown
-      '#CCD1D1' // Iron Gray
-    ]
-    // Define your color scheme
     return Object.entries(balancesByChain).map(([chain, balance], index) => ({
       name: `${chain.split(':')[0]}_${index}_${chain.split(':')[1]}`, // Add an index to make the key unique
       value: hidePrivateData ? 0 : baseToAsset(balance).amount().toNumber(),
-      fillColor: COLORS[index % COLORS.length]
+      fillColor: ChartColors[index % ChartColors.length]
     }))
   }, [balancesByChain, hidePrivateData])
 
