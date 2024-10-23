c9ba101d079122fcd0abeb1d6ed626a91713680a
GrassRootsCrypto <chris@grassrootscrypto.io>
2024-10-23 14:31:31 +1030

    part done, needs xchainjs update also

--- src/shared/api/types.ts
+++ src/shared/api/types.ts
@@ -24,7 +24,7 @@ export const thorDetails: Dex = {
   chain: THORChain,
   asset: AssetRuneNative,
   decimals: THORCHAIN_DECIMAL,
-  url: 'runescan.io'
+  url: 'thorchain.net'
 }
 export const mayaDetails: Dex = {
   chain: MAYAChain,
