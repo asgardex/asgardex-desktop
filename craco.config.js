16522aeb0f09dbac0b6d847f8bc5ce59761e2527
cinnamoroll6130 <nextmission4260@gmail.com>
2024-10-25 13:38:08 +0900

    feat: update craco config

--- craco.config.js
+++ craco.config.js
@@ -59,7 +59,13 @@ module.exports = {
             fullySpecified: false
           }
         }
-      ]
+      ].map((rule) => {
+        if (rule.oneOf instanceof Array) {
+          // eslint-disable-next-line no-param-reassign
+          rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.html$/, /\.json$/]
+        }
+        return rule
+      })
 
       return webpackConfig
     },
