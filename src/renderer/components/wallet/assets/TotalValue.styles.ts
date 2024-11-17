acc71590e6543f9f29b15fe3d9dc08ad5370c1f8
cinnamoroll6130 <nextmission4260@gmail.com>
2024-11-17 13:18:08 +0000

    chore: update styles for total value

--- src/renderer/components/wallet/assets/TotalValue.styles.ts
+++ src/renderer/components/wallet/assets/TotalValue.styles.ts
@@ -15,11 +15,7 @@ export const Container = styled.div`
 `
 
 export const Spin = styled(UISpin)`
-  padding-top: 10px;
-
-  ${media.sm`
-  padding-top: 20px;
-  `}
+  padding-top: 16px;
 `
 
 export const BalanceTitle = styled(UILabel)`
@@ -45,19 +41,11 @@ export const TitleContainer = styled.div`
 `
 
 export const BalanceLabel = styled(UILabel)`
-  font-size: 27px;
-  line-height: 100%;
+  margin: 16px 0px;
+  font-size: 28px;
+  color: ${palette('text', 2)};
   padding: 0 10px;
-  /* width: auto; */
   text-align: center;
-
-  ${media.sm`
-  font-size: 41px;
-  `}
-
-  ${media.lg`
-  font-size: 54px;
-  `}
 `
 
 export const BalanceError = styled(UILabel).attrs({
