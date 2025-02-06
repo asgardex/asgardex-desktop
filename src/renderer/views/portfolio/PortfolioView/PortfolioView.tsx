f20add791003fa8dd8e784fb369d41cebb7be084
cinnamoroll6130 <nextmission4260@gmail.com>
2025-02-06 03:05:38 +0000

    chore: fix issue

--- src/renderer/views/portfolio/PortfolioView/PortfolioView.tsx
+++ src/renderer/views/portfolio/PortfolioView/PortfolioView.tsx
@@ -56,7 +56,7 @@ import { balancesState$, getLedgerAddress$, reloadBalancesByChain } from '../../
 import { DEFAULT_BALANCES_FILTER, INITIAL_BALANCES_STATE } from '../../../services/wallet/const'
 import { useApp } from '../../../store/app/hooks'
 import { BaseAmountRD } from '../../../types'
-import { WalletAddressInfo } from '../../bonds/ThorBondsView'
+import { WalletAddressInfo } from '../../bonds/types'
 import { getValueOfRuneInAsset } from '../../pools/Pools.utils'
 import * as H from '../../wallet/PoolShareView.helper'
 import { getSaversTotal } from '../../wallet/SaversTableView.helper'
