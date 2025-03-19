7f3726331f7cf728d6e3c25bf2a18587cc57be49
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2025-03-19 10:05:07 +0930

    update with tests

--- src/renderer/helpers/evmHelper.ts
+++ src/renderer/helpers/evmHelper.ts
@@ -12,13 +12,18 @@ export const isEvmChain = (chain: Chain): boolean => {
   return isEthChain(chain) || isAvaxChain(chain) || isBscChain(chain) || isArbChain(chain) || isBaseChain(chain)
 }
 
+export const isEvmToken = (asset: AnyAsset): boolean => {
+  return !isEthAsset(asset) && !isAethAsset(asset) && !isAvaxAsset(asset) && !isBscAsset(asset) && !isBASEAsset(asset)
+}
+
 /**
  * Helper function to determine if the asset is not the chain/gas asset
  * @param asset - input
  * @returns - boolean if the asset is a erc/brc etc.
  */
-export const isEvmToken = (asset: AnyAsset): boolean => {
-  return !isEthAsset(asset) && !isAethAsset(asset) && !isAvaxAsset(asset) && !isBscAsset(asset) && !isBASEAsset(asset)
+export const isEvmChainToken = (asset: AnyAsset): boolean => {
+  const chain: Chain = asset.chain
+  return isEvmChain(chain) && isEvmToken(asset)
 }
 
 /**
