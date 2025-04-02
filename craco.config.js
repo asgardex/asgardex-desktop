87238a10273a378da3e5e28e4fa11d2875f693ce
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2025-04-02 10:57:11 +0930

    wip

--- craco.config.js
+++ craco.config.js
@@ -35,15 +35,7 @@ module.exports = {
         path: require.resolve('path-browserify'),
         fs: require.resolve('browserify-fs'),
         assert: require.resolve('assert'),
-        process: require.resolve('process/browser'),
-        dns: require.resolve('dns.js'),
-        zlib: require.resolve('browserify-zlib'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        net: false, // Stubbed; not needed in browser
-        tls: false, // Stubbed; not needed in browser
-        http2: false, // Stubbed; no full polyfill available
-        dgram: false
+        process: require.resolve('process/browser')
       }
 
       webpackConfig.ignoreWarnings = [
