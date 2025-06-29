8a3bb0c877020320cc80d8881faaccf1a1fb4928
dynst <148708712+dynst@users.noreply.github.com>
2025-06-29 00:00:00 +0000

    simplify instanceObservable to just a minimal Applicative1

--- src/renderer/helpers/rx/instanceObservable.ts
+++ src/renderer/helpers/rx/instanceObservable.ts
@@ -1,4 +1,4 @@
-import { type compactable, type monadTask, either as E, option as O } from 'fp-ts'
+import { type applicative } from 'fp-ts'
 import * as Rx from 'rxjs'
 import { Observable } from 'rxjs'
 import * as RxOp from 'rxjs/operators'
@@ -14,21 +14,9 @@ declare module 'fp-ts/HKT' {
   }
 }
 
-const filterMap = <A, B>(o: Observable<A>, f: (a: A) => O.Option<B>) =>
-  o.pipe(RxOp.mergeMap((a) => O.fold(() => Rx.EMPTY, Rx.of<B>)(f(a))))
-
-export const instanceObservable: monadTask.MonadTask1<URI> & compactable.Compactable1<URI> = {
+export const instanceObservable: applicative.Applicative1<URI> = {
   URI,
   map: (o, f) => o.pipe(RxOp.map(f)), // Functor
   ap: (oF, o) => Rx.combineLatest([oF, o]).pipe(RxOp.map(([f, a]) => f(a))), // Apply
-  of: Rx.of, // Applicative
-  chain: (o, f) => o.pipe(RxOp.mergeMap(f)), // Monad
-  fromIO: (f) => Rx.defer(() => Rx.of(f())), // MonadIO
-  fromTask: Rx.defer, // MonadTask
-  compact: (o) => filterMap(o, (a) => a), // Compactable
-  separate: (o) => ({
-    // Compactable
-    left: filterMap(o, (a) => O.fromEither(E.swap(a))),
-    right: filterMap(o, O.fromEither)
-  })
+  of: Rx.of // Applicative
 }
