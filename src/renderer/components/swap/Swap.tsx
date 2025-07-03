2d1ab0a16da89a940524e6734dde8741bb4a4347
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2025-07-03 11:35:30 +0930

    fix chainflip blank screen issue

--- src/renderer/components/swap/Swap.tsx
+++ src/renderer/components/swap/Swap.tsx
@@ -1090,7 +1090,9 @@ export const Swap = ({
       oQuoteProtocol,
       O.chain((txDetails) => {
         // Disable slippage protection temporary for Ledger/BTC (see https://github.com/thorchain/asgardex-electron/issues/2068)
-        return !disableSlippage && swapResultAmountMax.baseAmount.gt(zeroTargetBaseAmountMax1e8)
+        return !disableSlippage &&
+          swapResultAmountMax.baseAmount.gt(zeroTargetBaseAmountMax1e8) &&
+          txDetails.protocol !== 'Chainflip'
           ? O.some(Utils.getSwapLimit1e8(txDetails.memo))
           : O.none
       })
