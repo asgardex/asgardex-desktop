f1559b03369fef22d4ed77f0af8cbd91534cd7c1
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2025-10-16 11:07:04 +0930

    update mimir status to ready > 1

--- src/renderer/hooks/useMimirHalt.ts
+++ src/renderer/hooks/useMimirHalt.ts
@@ -25,8 +25,8 @@ import {
 export const getMimirStatus = (mimir = 0, lastHeight = 0) => {
   // no mimir -> no action
   if (mimir === 0) return false
-  // 1 -> halt | pause
-  if (mimir > 1) return true
+  // 1 or any numeric value -> halt | pause
+  if (mimir >= 1) return true
   // compare to current block height
   if (mimir < lastHeight) return true
   return false
