8398c80bd8a6f2bc7777493fd8e49bb6238b0b79
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2025-10-24 07:45:18 +0930

    trunk fmt

--- src/main/api/ledger/solana/address.ts
+++ src/main/api/ledger/solana/address.ts
@@ -40,7 +40,7 @@ export const getAddress = async (
   } catch (error) {
     return E.left({
       errorId: LedgerErrorId.GET_ADDRESS_FAILED,
-      msg: isError(error) ? error?.message ?? error.toString() : `${error}`
+      msg: isError(error) ? (error?.message ?? error.toString()) : `${error}`
     })
   }
 }
--- src/main/api/ledger/solana/transaction.ts
+++ src/main/api/ledger/solana/transaction.ts
@@ -55,7 +55,7 @@ export const send = async ({
   } catch (error) {
     return E.left({
       errorId: LedgerErrorId.SEND_TX_FAILED,
-      msg: isError(error) ? error?.message ?? error.toString() : `${error}`
+      msg: isError(error) ? (error?.message ?? error.toString()) : `${error}`
     })
   }
 }
