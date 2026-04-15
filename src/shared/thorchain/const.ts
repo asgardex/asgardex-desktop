dd066d74004accbd4f16da04d39ceaa9b9aa1ee9
Slambammer <thenothing07@gmail.com>
2026-04-15 09:15:48 +0100

    Update RPC and API URLs for Thorchain

--- src/shared/thorchain/const.ts
+++ src/shared/thorchain/const.ts
@@ -4,11 +4,11 @@ import { envOrDefault } from '../utils/env'
 export const DEFAULT_THORNODE_RPC_URLS: ApiUrls = {
   mainnet: envOrDefault(import.meta.env.VITE_MAINNET_THORNODE_RPC, 'https://rpc.ninerealms.com'),
   stagenet: envOrDefault(import.meta.env.VITE_STAGENET_THORNODE_RPC, 'https://stagenet-rpc.ninerealms.com'),
-  testnet: envOrDefault(import.meta.env.VITE_TESTNET_THORNODE_RPC, 'https://rpc.ninerealms.com')
+  testnet: envOrDefault(import.meta.env.VITE_TESTNET_THORNODE_RPC, 'https://gateway.liquify.com/chain/thorchain_rpc')
 }
 
 export const DEFAULT_THORNODE_API_URLS: ApiUrls = {
-  mainnet: envOrDefault(import.meta.env.VITE_MAINNET_THORNODE_API, 'https://thornode.ninerealms.com'),
+  mainnet: envOrDefault(import.meta.env.VITE_MAINNET_THORNODE_API, 'https://gateway.liquify.com/chain/thorchain_api'),
   stagenet: envOrDefault(import.meta.env.VITE_STAGENET_THORNODE_API, 'https://stagenet-thornode.ninerealms.com'),
   testnet: envOrDefault(import.meta.env.VITE_TESTNET_THORNODE_API, 'https://testnet.thornode.thorchain.info')
 }
