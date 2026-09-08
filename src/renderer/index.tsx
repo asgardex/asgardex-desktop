997fb92e7b350ed778fccab9293930713297ddfd
Naq302 <Nq-Osm1um@proton.me>
2026-09-08 15:14:05 +0930

    fix(oneclick): stamp referral in aggregator patch, drop fetch wrap

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -4,9 +4,6 @@ import './index.css'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
-import { installOneClickReferralStamp } from './services/oneclick/referralStamp'
-
-installOneClickReferralStamp()
 
 // // Registers custom headers (9R endpoints only)
 // register9Rheader('')
